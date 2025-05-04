22969e2dced18539adc2159eded7ac8f75d1ac29
Jie0526 <shenjieshenjie2023@163.com>
2025-05-05 01:08:35 +0800

    Update script.js

--- 3D website Template_MASTER/script.js
+++ 3D website Template_MASTER/script.js
@@ -1,50 +1,95 @@
-var scene, camera, renderer, box;
+var scene, camera, renderer, clock, mixer, actions = [], mode;
 
 init();
 
 function init(){
+
+    const assetPath = './'; // Path to assets
+
+    clock = new THREE.Clock();
+
+    // Create the scene
     scene= new THREE.Scene();
+    scene.background = new THREE.Color(0x00aaff);
 
-    scene.background = new THREE.Color(0xaaaaaa);
-    
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.z = 3;
+    // Set up the camera
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(-5, 25, 20);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-    const light = new THREE.DirectionalLight();
-    light.position.set(0, 1, 2);
+    // Add lighting
+    const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+    scene.add(ambient);
+    const light = new THREE.DirectionalLight(0xFFFFFF, 2);
+    light.position.set(0, 10, 2); 
     scene.add(light);
 
-    const material = new THREE.MeshStandardMaterial({color:new THREE.Color('skyblue')});
+    // Set up the renderer
+    const canvas = document.getElementById('threeContainer');
+    renderer = new THREE.WebGLRenderer({canvas:canvas});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    resize();
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    box = new THREE.Mesh(geometry,material);
-    box.position.x = 0;
-    scene.add(box);
+    // Add OrbitControls
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.target.set(1, 2, 0);
+    controls.update();
 
-    window.addEventListener('resize',onResize, false);
+    // Button to control animations
+    mode = 'open';
+    const btn = document.getElementById("btn");
+    btn.addEventListener('click', function() {
+        if (actions.length > 0) {
+            if (mode === "open") {
+                actions.forEach(action => {
+                    action.timeScale = 1;
+                    action.reset();
+                    action.play();
+                });
+            }
+        }
+    });
 
-    update();
+    // Load the glTF model
+    const loader = new THREE.GLTFLoader();
+    loader.load(assetPath + 'assets/cola_open_can.glb', function(gltf) {
+        const model = gltf.scene;
+        scene.add(model);
+    
+        // Set up animations 
+        mixer = new THREE.AnimationMixer(model);
+        const animations = gltf.animations; 
+        animations.forEach(clip => {
+            const action = mixer.clipAction(clip);
+            actions.push(action);
+        });
+    });
+
+    // Handle resizing
+    window.addEventListener('resize',resize, false);
+
+    // Start the animation loop
+    animate();
 
 }
 
-function update(){
-
-    requestAnimationFrame(update);
-
-    box.rotation.y += 0.01;
-
-    // Render the scene
+function animate() {
+    requestAnimationFrame(animate);
+  
+    // Update animations
+    if (mixer) {
+      mixer.update(clock.getDelta());
+    }
+  
     renderer.render(scene, camera);
-
 }
+  
+function resize() {
+    const canvas = document.getElementById('threeContainer');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-function onResize(){
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = width / height;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
+    renderer.setSize(width, height);
 }
+  

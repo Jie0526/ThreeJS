c336d479f6b5a2aa22a6f3220076cc231f3039f7
Jie0526 <shenjieshenjie2023@163.com>
2025-05-07 18:31:57 +0800

    Update coke_script.js

--- 3D website/coke_script.js
+++ 3D website/coke_script.js
@@ -320,6 +320,13 @@ function init(){
 
     // Start the animation loop
     animate();
+
+    // Add Reset button logic
+    const resetBtn = document.getElementById("resetModel");
+    resetBtn.addEventListener('click', function () {
+        loadModel('assets/cola_open_can.glb');
+    });
+
 }
 
 function toggleWireframe(enable){
